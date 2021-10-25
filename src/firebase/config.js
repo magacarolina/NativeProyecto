0569d5f4f63cdfbdfa9545fd71cf938ef7e5486d
nicolesuchmon <nickysuchmon@gmail.com>
2021-10-25 11:20:59 -0300

    implementando firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import app from 'firebase/app';
+import firebase from 'firebase';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBNWH9BBLUIAEVvnPumnmgGdh-ze6uHy0",
+  authDomain: "nativeproyecto.firebaseapp.com",
+  projectId: "nativeproyecto",
+  storageBucket: "nativeproyecto.appspot.com",
+  messagingSenderId: "58915979273",
+  appId: "1:58915979273:web:05c64c916d526ce7e0b8d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const storage = app.storage();
+export const db = app.firestore();
